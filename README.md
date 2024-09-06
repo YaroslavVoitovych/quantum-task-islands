01b7642fd6c57c7fa5f02b7244b5e26cdfab4fce
Yaroslav_Voitovych2236@jabil.com-at-648436754016 <Yaroslav_Voitovych2236@jabil.com>
2024-09-06 18:18:37 +0300

    Initial commit. Added solution, tests and README instructions

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # quantum-task-islands
 Islands counting algorithm
+
+This code was tested for Python3.12.0 on MacOS
+Steps to reproduce:
+1. python3 -m venv .env
+2. source .env/bin/activate
+3. python3 -m pip install -r requirements.txt
+4. pytest .
---
+++ test_islands.py
@@ -0,0 +1,72 @@
+from islands_counter import islands_counter
+
+
+
+def test_1():
+    # Inputs
+    M = 9
+    N = 10
+    A = [
+        [0 for i in range(N)] for j in range(M)
+    ]
+    A[0][-1] = 1
+    A[5][0] = 1
+    A[6][0] = 1
+    A[-1][1] = 1
+    A[-1][2] = 1
+    A[-1][3] = 1
+    A[-2][2] = 1
+    A[6][-1] = 1
+    A[2][3] = 1
+    A[3][3] = 1
+    A[4][3] = 1
+    A[4][2] = 1
+    A[4][4] = 1
+    A[4][5] = 1
+    A[3][4] = 1
+    A[3][5] = 1
+    A[2][5] = 1
+    A[5][6] = 1
+    A[6][6] = 1
+    assert islands_counter(A, N, M) == 6
+
+def test_2():
+    N = 3
+    M = 3
+    A = [[0, 1, 0],
+         [0, 0, 0],
+         [0, 1, 1]]
+    assert islands_counter(A, N, M) == 2
+
+def test_3():
+    N = 10
+    M = 10
+    A = [
+        [1 for i in range(N)] for j in range(M)
+    ]
+    assert islands_counter(A, N, M) == 1
+
+def test_4():
+    M = 3
+    N = 4
+    A = [[0, 0, 0, 1],
+        [0, 0, 1, 0],
+        [0, 1, 0, 0]]
+    assert islands_counter(A, N, M) == 3
+
+def test_5():
+    M = 3
+    N = 4
+    A = [[1, 1, 1, 1],
+        [0, 0, 0, 1],
+        [1, 1, 1, 1]]
+    assert islands_counter(A, N, M) == 1
+
+def test_6():
+    M = 3
+    N = 4
+    A = [[1, 1, 1, 1],
+        [1, 0, 0, 1],
+        [1, 1, 1, 1]]
+    assert islands_counter(A, N, M) == 1
+
